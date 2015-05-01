fbab2fbcd9b30052520a50a44ae19d5db957ef93
utkuolcar <utkuolcar@gmail.com>
2015-05-02 01:37:46 +0300

    Initial Commit

---
+++ Vector2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolidGeometry
+{
+    public class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Vector2(float x,float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
