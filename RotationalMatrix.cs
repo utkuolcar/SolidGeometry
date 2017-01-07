8b7e59d497530665de0364470865075bf817d1b3
Utku Olcar <utkuolcar@gmail.com>
2017-01-07 22:23:27 +0300

    Operations and Functions Adding

--- RotationalMatrix.cs
+++ RotationalMatrix.cs
@@ -18,6 +18,22 @@ namespace SolidGeometry
             Column2 = column2;
             Column3 = column3;
         }
+        public RotationalMatrix(double roll, double pitch, double yaw)
+        {
+            Column1 = new Vector3(
+                (float)(Math.Cos(roll) * Math.Cos(pitch) * Math.Cos(yaw) - Math.Sin(roll) * Math.Sin(yaw)),
+                (float)(Math.Sin(roll) * Math.Cos(pitch) * Math.Cos(yaw) + Math.Cos(roll) * Math.Sin(yaw)),
+                (float)(-Math.Sin(pitch)* Math.Cos(yaw)));
+            Column2 = new Vector3(
+                (float)(-Math.Cos(roll) * Math.Cos(pitch) * Math.Sin(yaw) - Math.Sin(roll) * Math.Cos(yaw)),
+                (float)(-Math.Sin(roll) * Math.Cos(pitch) * Math.Sin(yaw) + Math.Cos(roll) * Math.Cos(yaw)),
+                (float)(Math.Sin(pitch) * Math.Sin(yaw)));
+            Column3 = new Vector3(
+                (float)(Math.Cos(roll) * Math.Cos(pitch) ),
+                (float)(Math.Sin(roll) * Math.Cos(pitch)),
+                (float)(Math.Cos(pitch)));
+
+        }
         public RotationalMatrix InverseMatrix()
         {
             RotationalMatrix matrix = GetCofactor();
@@ -35,6 +51,31 @@ namespace SolidGeometry
         public static Vector3 MultiplyWith3x1(RotationalMatrix matrix, Vector3 vector)
         {
             return new Vector3((matrix.Column1.X * vector.X) + (matrix.Column2.X * vector.Y) + (matrix.Column3.X * vector.Z), (matrix.Column1.Y * vector.X) + (matrix.Column2.Y * vector.Y) + (matrix.Column3.Y * vector.Z), (matrix.Column1.Z * vector.X) + (matrix.Column2.Z * vector.Y) + (matrix.Column3.Z * vector.Z));
+        }
+        public static RotationalMatrix identityMatrix()
+        {
+            return new RotationalMatrix(new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1));
+        }
+        public static Vector3 operator *(RotationalMatrix r, Vector3 v)
+        {
+            return MultiplyWith3x1(r, v);
+        }
+        public static RotationalMatrix operator *(RotationalMatrix r1, RotationalMatrix r2)
+        {
+            Vector3 c0 = new Vector3(
+                r1.Column1.X * r2.Column1.X + r1.Column2.X * r2.Column1.Y + r1.Column3.X * r2.Column1.Z, 
+                r1.Column1.Y * r2.Column1.X + r1.Column2.Y * r2.Column1.Y + r1.Column3.Y * r2.Column1.Z, 
+                r1.Column1.Z * r2.Column1.X + r1.Column2.Z * r2.Column1.Y + r1.Column3.Z * r2.Column1.Z);
+            Vector3 c1 = new Vector3(
+                r1.Column1.X * r2.Column2.X + r1.Column2.X * r2.Column2.Y + r1.Column3.X * r2.Column2.Z, 
+                r1.Column1.Y * r2.Column2.X + r1.Column2.Y * r2.Column2.Y + r1.Column3.Y * r2.Column2.Z, 
+                r1.Column1.Z * r2.Column2.X + r1.Column2.Z * r2.Column2.Y + r1.Column3.Z * r2.Column2.Z);
+            Vector3 c2 = new Vector3(
+                r1.Column1.X * r2.Column3.X + r1.Column2.X * r2.Column3.Y + r1.Column3.X * r2.Column3.Z, 
+                r1.Column1.Y * r2.Column3.X + r1.Column2.Y * r2.Column3.Y + r1.Column3.Y * r2.Column3.Z, 
+                r1.Column1.Z * r2.Column3.X + r1.Column2.Z * r2.Column3.Y + r1.Column3.Z * r2.Column3.Z);
+
+            return new RotationalMatrix(c0,c1,c2);
         }
         public RotationalMatrix GetCofactor()
         {
--- TransformMatrix.cs
+++ TransformMatrix.cs
@@ -15,5 +15,45 @@ namespace SolidGeometry
             this.RotationMatrix = rotation;
             this.Origin = o;
         }
+        public TransformMatrix(double a, double alpha, double d, double theta)
+        {
+            double ca = Math.Cos(alpha);
+            double sa = Math.Sin(alpha);
+            double ct = Math.Cos(theta);
+            double st = Math.Sin(theta);
+
+            //T =[ct - st 0 a ; st* ca ct* ca -sa - sa * d; st* sa ct* sa ca ca*d; 0 0 0 1];
+            Vector3 c0 = new Vector3((float)ct, (float)(st *ca), (float)(st*sa));
+            Vector3 c1 = new Vector3((float)-st, (float)(ct * ca), (float)(ct * sa));
+            Vector3 c2 = new Vector3(0f, (float)-sa, (float)ca);
+
+            
+            this.RotationMatrix = new RotationalMatrix(c0,c1,c2);
+            this.Origin = new Vector3((float)a, (float)(-sa * d), (float)(ca * d));
+        }
+        public TransformMatrix inverse() {
+            RotationalMatrix inverseRotation = RotationMatrix.TransposeMatrix();
+            return new TransformMatrix(inverseRotation, inverseRotation * Origin.GetMinus());
+        }
+        public static TransformMatrix identityMatrix() {
+            return new TransformMatrix(new RotationalMatrix(new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1)), new Vector3(0, 0, 0));
+        }
+        public static TransformMatrix operator *(TransformMatrix t1, TransformMatrix t2)
+        {    
+            Vector3 o = new Vector3(
+                t1.RotationMatrix.Column1.X * t2.Origin.X + t1.RotationMatrix.Column2.X * t2.Origin.Y + t1.RotationMatrix.Column3.X * t2.Origin.Z + t1.Origin.X,
+                t1.RotationMatrix.Column1.Y * t2.Origin.X + t1.RotationMatrix.Column2.Y * t2.Origin.Y + t1.RotationMatrix.Column3.Y * t2.Origin.Z + t1.Origin.Y,
+                t1.RotationMatrix.Column1.Z * t2.Origin.X + t1.RotationMatrix.Column2.Z * t2.Origin.Y + t1.RotationMatrix.Column3.Z * t2.Origin.Z + t1.Origin.Z);
+
+            return new TransformMatrix(t1.RotationMatrix*t2.RotationMatrix,o);
+        }
+        public static Vector3 operator *(TransformMatrix t1, Vector3 v)
+        {
+           return new Vector3(
+                t1.RotationMatrix.Column1.X * v.X + t1.RotationMatrix.Column2.X * v.Y + t1.RotationMatrix.Column3.X * v.Z + t1.Origin.X,
+                t1.RotationMatrix.Column1.Y * v.X + t1.RotationMatrix.Column2.Y * v.Y + t1.RotationMatrix.Column3.Y * v.Z + t1.Origin.Y,
+                t1.RotationMatrix.Column1.Z * v.X + t1.RotationMatrix.Column2.Z * v.Y + t1.RotationMatrix.Column3.Z * v.Z + t1.Origin.Z);
+
+        }
     }
 }
